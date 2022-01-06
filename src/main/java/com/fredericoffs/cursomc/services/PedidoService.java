357025029adc2ab4478b2befdfeacf82bdfe108b
Frederico F Silva <fredericoffs@live.com>
2022-01-06 16:49:39 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/fredericoffs/cursomc/services/PedidoService.java
+++ src/main/java/com/fredericoffs/cursomc/services/PedidoService.java
@@ -38,6 +38,9 @@ public class PedidoService {
 	@Autowired
 	private ClienteService clienteService;
 
+	@Autowired
+	private EmailService emailService;
+
 	public Pedido findById(Integer id) {
 		Optional<Pedido> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
@@ -72,7 +75,7 @@ public class PedidoService {
 
 		itemPedidoRepository.saveAll(obj.getItens());
 
-		System.out.println(obj);
+		emailService.sendOrderConfirmationEmail(obj);
 
 		return obj;
 	}
